a3fd1f36fab8f7e085820af464f189c454ee0789
BtramVu <bbbvut@gmail.com>
2021-01-14 16:45:22 -0800

    install npm inquirer and add required packages to index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+// // TODO: Include packages needed for this application
+// const inquirer = require('inquirer');
+// const fs = require('fs');
+// const util = require('util');
+// const generateMarkdown = require('./utils/generateMarkdown.js');
+
+// // TODO: Create an array of questions for user input
+// const questions = [
+  
+// ];
+
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {  
+
+//   // bai so 13
+//   fs.readFile('data.csv', 'utf8', (error, data) =>
+//   error ? console.error(error) : console.log(data)
+// );
+// // bai cua ban Tran
+//   fs.writeFile(fileName, data, err => {
+//   if (err) {
+//     return console.log(err);
+//   } console.log("Success!")
+// });
+// }
+
+// // TODO: Create a function to initialize app
+// function init() {}
+
+// // Function call to initialize app
+// init();
